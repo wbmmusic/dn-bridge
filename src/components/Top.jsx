72505307f8b4d28d5485823f7d47e69810c6a05f
Marece <wbmmusic@gmail.com>
2022-01-18 13:22:58 -0600

    0.0.3

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './Home'
+import Updates from '../Updates'
 
 export default function Top() {
     return (
         <div>
             <Home />
+            <Updates />
         </div>
     )
 }
