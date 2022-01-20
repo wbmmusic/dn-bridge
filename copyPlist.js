d01ef84122c7462e82467b58c9e089aabebad80a
Marece <wbmmusic@gmail.com>
2022-01-20 09:11:08 -0600

    0.0.12

--- copyPlist.js
+++ copyPlist.js
@@ -1,4 +1,4 @@
-const { copyFileSync } = require('fs')
+const { cpSync } = require('fs')
 const { join } = require('path')
 
-copyFileSync(join(__dirname, 'entitlements.mac.plist'), join(__dirname, 'build', 'entitlements.mac.plist'))
+cpSync(join(__dirname, 'entitlements.mac.plist'), join(__dirname, 'build', 'entitlements.mac.plist'), { recursive: true })
